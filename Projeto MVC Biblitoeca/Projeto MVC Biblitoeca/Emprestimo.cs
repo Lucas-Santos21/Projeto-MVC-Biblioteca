3e60bb87a55622f5f3dd70c4728e73ae6e0d2b91
Lucas Santos <lucassantos135b211@gmail.com>
2025-10-02 17:13:32 -0300

    classes criadas

---
+++ Projeto MVC Biblitoeca/Projeto MVC Biblitoeca/Emprestimo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_MVC_Biblitoeca
+{
+    class Emprestimo
+    {
+        //propriedades
+
+        private DateTime _dtEmprestimo;
+        private DateTime? _dtDevolucao;
+
+        //Construtor
+
+        public Emprestimo(DateTime dtEmprestimo, DateTime? dtDevolucao = null)
+        {
+            DtEmprestimo = dtEmprestimo;
+            DtDevolucao = dtDevolucao;
+        }
+
+        //Getters e Setters
+
+        public DateTime DtEmprestimo
+        {
+            get { return _dtEmprestimo; }
+            set { _dtEmprestimo = value; }
+        }
+
+        public DateTime? DtDevolucao
+        {
+            get { return _dtDevolucao; }
+            set { _dtDevolucao = value; }
+        }
+
+    }
+}
---
+++ Projeto MVC Biblitoeca/Projeto MVC Biblitoeca/Exemplar.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_MVC_Biblitoeca
+{
+    class Exemplar
+    {
+        //propriedades
+        private int _tombo;
+        private List<Emprestimo> _emprestimos = new List<Emprestimo>();
+
+        //Construtor
+        public Exemplar(int tombo)
+        {
+            Tombo = tombo;
+        }
+
+        //getters e setters
+        public int Tombo
+        {
+            get { return _tombo; }
+            set { _tombo = value; }
+        }
+
+        public List<Emprestimo> Emprestimos
+        {
+            get { return _emprestimos; }
+            set { _emprestimos = value; }
+        }
+
+        //Metodos publicos
+        public bool emprestar()
+        {
+            bool saida = false;
+
+            if(disponivel() == true)
+            {
+                DateTime dtEmprestimo = DateTime.Now;
+
+                Emprestimos.Add(new Emprestimo(dtEmprestimo));
+
+                saida = true;
+            }
+
+            return saida;
+
+        }
+
+        public bool disponivel()
+        {
+            bool saida = false;
+
+            if(Emprestimos.Count == 0)
+            {
+                saida = true;
+            }
+            else
+            {
+                Emprestimo ultimo = Emprestimos.Last();
+
+                if (ultimo.DtDevolucao != null)
+                {
+                    saida = true;
+                }
+
+            }
+
+            return saida;
+
+        }
+
+    }
+}
